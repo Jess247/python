f2697e592eefa9862e9ae8f7e191d29614704108
Jess247 <jesss8692@hotmail.com>
2023-02-07 17:35:12 +0100

    update file

--- python/atbsPy/regex_1.py
+++ python/atbsPy/regex_1.py
@@ -1,1 +1,19 @@
-# is input a phone number without regular expressions
+# is input a phone number WITHOUT regular expressions
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isDecimal():
+            return False
+    if text[3] != "-":
+        return False
+    for i in range(4, 7):
+        if not text[i].isDecimal():
+            return False
+    if text[7] != "-":
+        return False
+    for i in range(8, 12):
+        if not text[i].isDecimal():
+            return False
+    return True
