ac349bd2649c6654d5e74cf50da2e0960996faef
Jess247 <jesss8692@hotmail.com>
2023-02-15 18:11:34 +0100

    update file

--- python/atbsPy/regex_8.py
+++ python/atbsPy/regex_8.py
@@ -11,3 +11,52 @@ print(vowels.findall('RoboCop eats baby food. BABY FOOD'))
 # find everything but vowels
 noVowels = re.compile('[^aeiouAEIOU]')
 print(noVowels.findall('RoboCop eats baby food. BABY FOOD'))
+# string has to start with a match
+startsWith = re.compile(r'^hello')
+print('startsWith: ', startsWith.search('hello, world.'))
+print('startsWith: ', startsWith.search('She said hello, world.'))
+# string has to end with a match
+endsWith = re.compile(r'\d$')
+print('endswWith: ', endsWith.search('i am 30'))
+print('endswWith: ', endsWith.search('i am jess'))
+# beginning and end has to match
+wholeStringNum = re.compile(r'^\d+$')
+print('wholeStringNum: ', wholeStringNum.search("12346"))
+print('wholeStringNum: ', wholeStringNum.search("123hts46"))
+# joker .
+joker = re.compile(r'.at')
+print('joker: ', joker.findall('The cat in rhe hat sat on a flat mat'))
+# find any (.*) is always greedy (.*?) not greedy
+nameReg = re.compile(r'First name: (.*) Last name: (.*)')
+mo = nameReg.search('First name: Jess Last name: Sample')
+print('nameReg: ', mo.group(1), mo.group(2))
+# not greedy
+nameRegNotGreedy = re.compile(r'<.*?>')
+mo = nameRegNotGreedy.search('<First name: Jess> Last name: Sample>')
+print('nameRegNotGreedy: ', mo.group())
+# DOTALL finds matches even if theres a linebreak
+dotall = re.compile('.*', re.DOTALL)
+mo = dotall.search('Hello,\nworld!')
+print('nameRegNotGreedy: ', mo.group())
+# substitude
+nameReg = re.compile(r'Agent \w+')
+print('nameReg: ', nameReg.sub('CENSORED',
+      'Agent Alice gave the secret documents to Agent Bob'))
+# integrate spaces and comments with re.VERBOSE
+complexReg = re.compile(r'''(
+(\d{3}|\(d{3}\))?
+# prefix
+(\s|-|\.)?
+# separator
+\d{3}
+# first three digits
+(\s|-|\.)
+# separator
+\d{4}
+# last four digits
+(\s*
+(ext|x|ext.)\s*\d{2,5})?
+# extension
+)''', re.VERBOSE)
+# add more than one argumemt to re.compile()
+argsReg = re.compile("foo", re.IGNORECASE | re.DOTALL | re.VERBOSE)
