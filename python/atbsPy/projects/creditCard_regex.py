55464147461d9e755454cb0b6926c34f8bfa74e4
Jess247 <jesss8692@hotmail.com>
2023-02-19 14:13:43 +0100

    add file

---
+++ python/atbsPy/projects/creditCard_regex.py
@@ -0,0 +1,11 @@
+''' This program will find credit card numbers in a text form the clipboard. 
+    All found credit card numbers will be censored.
+    The new text will be saved to the clipboard.'''
+import re
+import pyperclip
+
+# TODO: credit card regex
+
+# TODO: find matches in clipboard text and censor them
+
+# TODO: copy result to clipboard
