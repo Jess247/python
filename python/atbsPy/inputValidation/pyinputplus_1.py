ce07d1aa04d0b95dd43371ff53fb6cd517511b6a
Jess247 <jesss8692@hotmail.com>
2023-02-21 22:35:24 +0100

    update file

--- python/atbsPy/inputValidation/pyinputplus_1.py
+++ python/atbsPy/inputValidation/pyinputplus_1.py
@@ -1,3 +1,4 @@
 import pyinputplus as pyip
-response = pyip.inputNum("Enter a number, please.")
+response = pyip.inputNum(prompt="Enter a number, please.\n")
 print("You entered the number: ", response)
+# help(pyip.inputChoice)
