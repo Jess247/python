c8b08980b6aedc20e387483d46b41e714f18bdf2
Jess247 <jesss8692@hotmail.com>
2023-02-22 11:57:12 +0100

    update file

--- python/atbsPy/inputValidation/pyinputplus_1.py
+++ python/atbsPy/inputValidation/pyinputplus_1.py
@@ -1,4 +1,15 @@
 import pyinputplus as pyip
 response = pyip.inputNum(prompt="Enter a number, please.\n")
 print("You entered the number: ", response)
-# help(pyip.inputChoice)
+
+# input range mith arguments min, max greaterThan and lessThan
+response = pyip.inputNum("Enter num: ", min=4)
+print("You entered a number thats minimum 4: ", response)
+response = pyip.inputNum("Enter num: ", greaterThan=4)
+print("You entered a number thats greater than 4: ", response)
+response = pyip.inputNum("Enter num: ", min=4, lessThan=6)
+print("You entered a number thats greater than 4: ", response)
+
+# allow blank values input is optional
+response = pyip.inputNum(blank=True)
+print(response)
