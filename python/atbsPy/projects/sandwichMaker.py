262efa920fbeff4d8423b84812ea570ca8233131
Jess247 <jesss8692@hotmail.com>
2023-02-23 20:23:29 +0100

    update file

--- python/atbsPy/projects/sandwichMaker.py
+++ python/atbsPy/projects/sandwichMaker.py
@@ -3,16 +3,34 @@
 # all inputs need to be valid answers
 import pyinputplus as pyip
 # Ask user what to put on the sandwich
-bread = pyip.inputMenu(
-    "Bread Menu:\n", ["White bread", "Wheat bread", "Whole grain bread"])
-mainTopping = pyip.inputMenu(
-    "Toppings Menu:\n", ["Chicken", "Turkey", "Ham", "Tofu"])
+bread = pyip.inputMenu(["White bread", "Wheat bread", "Whole grain bread"])
+mainTopping = pyip.inputMenu(["Chicken", "Turkey", "Ham", "Tofu"])
 cheeseYesNo = pyip.inputYesNo("Would you like some cheese?")
-cheeseType = pyip.inputMenu(
-    "Cheese Menu:\n", ["Cheddar", "Gouda", "Mozzarella"])
+if cheeseYesNo == "yes":
+    cheeseYesNo = pyip.inputMenu(["Cheddar", "Gouda", "Mozzarella"])
+else:
+    cheeseYesNo = "no cheese"
 mayoYesNo = pyip.inputYesNo("Would you like some mayo?")
-lettuceYEsNo = pyip.inputYesNo("How about some lettuce?")
-tomatoYesNo = pyip.inputYesNo("Tomatos?")
-amountOfSandwiches = pyip.inputInt()
+if mayoYesNo == "yes":
+    mayoYesNo = "mayo"
+else:
+    mayoYesNo = "no mayo"
+lettuceYesNo = pyip.inputYesNo("How about some lettuce?")
+if lettuceYesNo == "yes":
+    lettuceYesNo = "lettuce"
+else:
+    lettuceYesNo = "no lettuce"
+tomatoYesNo = pyip.inputYesNo("Any tomatoes?")
+if tomatoYesNo == "yes":
+    tomatoYesNo = "tomatoes"
+else:
+    tomatoYesNo = "no tomatoes"
+amountOfSandwiches = pyip.inputInt("How many sandwiches do you want?", min=1)
 price = 5
 # TODO: put the sandwiches together
+
+print(
+    f"You ordered {bread} with {mainTopping}, {cheeseYesNo}, {mayoYesNo}, {lettuceYesNo} and {tomatoYesNo}.")
+if amountOfSandwiches > 1:
+    print(
+        f"You ordered {amountOfSandwiches}, that makes {price * amountOfSandwiches}$, please.")
