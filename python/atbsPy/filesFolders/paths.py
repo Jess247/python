9b4aa66c4c02437611116d1e4b83d7f487935a06
Jess247 <jesss8692@hotmail.com>
2023-02-27 15:41:55 +0100

    update file

--- python/atbsPy/filesFolders/paths.py
+++ python/atbsPy/filesFolders/paths.py
@@ -12,12 +12,20 @@ print(homeFolder / subfolder)
 # current working directory
 print(Path.cwd())
 # change current working directory
-os.chdir('/User/sample')  # throws error because folder doesn't exist
+# os.chdir('/User/sample')  # throws error because folder doesn't exist
 # user directory
 print(Path.home)
 # make multiple directories
-os.makedirs('/Users/sample/test/foldr')
+# os.makedirs('/Users/sample/test/foldr')
 # single directory
-Path('/User/sample').mkdir()
+# Path('/User/sample').mkdir()
 # absolute path true/false
-Path.cwd().is_absolute()
+Path.cwd().is_absolute()  # True
+Path('spa,/bacon/eggs').is_absolute()  # False
+# os functions
+print(os.path.abspath('.'))
+print(os.path.abspath('./scripts'))
+print(os.path.isabs('.'))
+print(os.path.isabs(os.path.abspath('.')))
+print(os.path.relpath('/user/mac', '/user'))
+print(os.path.relpath('/user', '/user/spam/eggs'))
