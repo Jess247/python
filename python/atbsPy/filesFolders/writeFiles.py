77f731bf7f8b6a6fa3e1283b1f9da3519b6305ce
Jess247 <61388219+Jess247@users.noreply.github.com>
2023-03-02 22:16:22 +0100

    Update writeFiles.py

--- python/atbsPy/filesFolders/writeFiles.py
+++ python/atbsPy/filesFolders/writeFiles.py
@@ -2,5 +2,12 @@ from pathlib import Path
 
 baconFile = open('bacon.txt', 'w')
 # write file object
-fileContent = baconFile.write('Hello, world!\n')
+baconFile.write('Hello, world!\n')
 baconFile.close()
+baconFile = open('bacon.txt', 'a')
+baconFile.write('Bacon is not a vegetable')
+baconFile.close()
+baconFile = open('bacon.txt')
+content = baconFile.read()
+baconFile.close()
+print(content)
