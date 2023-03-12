4ca32a7c64c8b351cb01493ca6bc7a9bf0605fd3
Jess247 <jesss8692@hotmail.com>
2023-03-12 21:34:44 +0100

    bug fix

---
+++ python/atbsPy/filesFolders/shelf.py
@@ -0,0 +1,16 @@
+import shelve
+# save data with shelve (like in a dict)
+shelfFile = shelve.open('mydata')
+cats = ['Zophie', 'Pooka', 'Simon']
+shelfFile['cats'] = cats
+shelfFile.close()
+# does the same:
+# cats = ['Zophie', 'Pooka', 'Simon']
+# with shelve.open as shelfFile:
+# shelfFile['cats'] = cats
+# shelfFile.close()
+
+# read shelve file
+shelfFile = shelve.open('mydata')
+type(shelfFile)
+print(shelfFile['cats'])
