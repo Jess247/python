fc01fc4faf1c8156800dc93e5fdf523dee78e7ba
Jess247 <jesss8692@hotmail.com>
2023-03-12 22:07:47 +0100

    add file

---
+++ python/atbsPy/projects/randomQuizGenerator.py
@@ -0,0 +1,7 @@
+# this program will generate a quiz with questions and answers in a random order.
+#  It will also create the solutions to the quizze
+
+# We need 35 quizzes
+# 50 multiple choice questions
+# each question needs the correct answer and three random false answers
+# 35 text files for the questions and 35 for the solutions
