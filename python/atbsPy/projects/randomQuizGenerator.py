63ad5bbdc76bc18dc1591861f16b922e6df54d20
Jess247 <jesss8692@hotmail.com>
2023-03-13 20:19:14 +0100

    add file

--- python/atbsPy/projects/randomQuizGenerator.py
+++ python/atbsPy/projects/randomQuizGenerator.py
@@ -1,3 +1,4 @@
+#! python3
 # this program will generate a quiz with questions and answers in a random order.
 #  It will also create the solutions to the quizze
 
@@ -5,3 +6,81 @@
 # 50 multiple choice questions
 # each question needs the correct answer and three random false answers
 # 35 text files for the questions and 35 for the solutions
+
+import random
+# Question data - key is the state, value the capital
+capitals = {
+    'Alabama': 'Montgomery',
+    'Alaska': 'Juneau',
+    'Arizona': 'Phoenix',
+    'Arkansas': 'Little Rock',
+    'California': 'Sacramento',
+    'Colorado': 'Denver',
+    'Connecticut': 'Hartford',
+    'Delaware': 'Dover',
+    'Florida': 'Tallahassee',
+    'Georgia': 'Atlanta',
+    'Hawaii': 'Honolulu',
+    'Idaho': 'Boise',
+    'Illinois': 'Springfield',
+    'Indiana': 'Indianapolis',
+    'Iowa': 'Des Moines',
+    'Kansas': 'Topeka',
+    'Kentucky': 'Frankfort',
+    'Louisiana': 'Baton Rouge',
+    'Maine': 'Augusta',
+    'Maryland': 'Annapolis',
+    'Massachusetts': 'Boston',
+    'Michigan': 'Lansing',
+    'Minnesota': 'St. Paul',
+    'Mississippi': 'Jackson',
+    'Missouri': 'Jefferson City',
+    'Montana': 'Helena',
+    'Nebraska': 'Lincoln',
+    'Nevada': 'Carson City',
+    'New Hampshire': 'Concord',
+    'New Jersey': 'Trenton',
+    'New Mexico': 'Santa Fe',
+    'New York': 'Albany',
+    'North Carolina': 'Raleigh',
+    'North Dakota': 'Bismarck',
+    'Ohio': 'Columbus',
+    'Oklahoma': 'Oklahoma City',
+    'Oregon': 'Salem',
+    'Pennsylvania': 'Harrisburg',
+    'Rhode Island': 'Providence',
+    'South Carolina': 'Columbia',
+    'South Dakota': 'Pierre',
+    'Tennessee': 'Nashville',
+    'Texas': 'Austin',
+    'Utah': 'Salt Lake City',
+    'Vermont': 'Montpelier',
+    'Virginia': 'Richmond',
+    'Washington': 'Olympia',
+    'West Virginia': 'Charleston',
+    'Wisconsin': 'Madison',
+    'Wyoming': 'Cheyenne'
+}
+
+# add 35 individual quizzes
+for quizNum in range(35):
+    # create files for questions and answers
+    quizFile = open(f'capitalquiz{quizNum + 1}.txt', 'w')
+    answerKeyFile = open(f'capitalquiz_answers{quizNum + 1}.txt', 'w')
+    # add head for the quiz
+    quizFile.write('Name:\n\nDate:\n\nPeriod:\n\n')
+    quizFile.write((' ' * 20) + f'State Capitals Quiz (From{quizNum + 1}')
+    quizFile.write('\n\n')
+    # shuffle states in random order
+    states = list(capitals.keys())
+    random.shuffle(states)
+    # add a question for all 50 states
+    for questionNum in range(50):
+        # true and false answers
+        correctAnswer = capitals[states[questionNum]]
+        wrongAnswers = list(capitals.values())
+        del wrongAnswers[wrongAnswers.index(correctAnswer)]
+        wrongAnswers = random.sample(wrongAnswers, 3)
+        answerOptions = wrongAnswers + random.shuffle(answerOptions)
+        # TODO: write questions and answers in quiz file
+        # TODO: write solution in file
