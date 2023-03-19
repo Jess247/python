2e2eb6ed1d58ed80c67f968e1c0f631b5c108f58
Jess247 <jesss8692@hotmail.com>
2023-03-19 17:43:55 +0100

    update file

--- python/atbsPy/projects/mcb.py
+++ python/atbsPy/projects/mcb.py
@@ -3,6 +3,7 @@
 # py.exe mcb.py save <key> - saves text with key to clipboard
 # py.exe mcb.py <key> - loads text saved in clipboard
 # py.exe mcb.py list - loads all keys to the clipboard
+# py.exe mcb.py delete <key> - deletes text and leyword
 import shelve
 import pyperclip
 import sys
@@ -12,10 +13,15 @@ mcbSelf = shelve.open('mcb')
 # save contents to clipboard
 if len(sys.argv) == 3 and sys.argv[1].lower() == 'save':
     mcbSelf[sys.argv[2]] = pyperclip.paste()
+elif len(sys.argv) == 3 and sys.argv[1].lower() == 'delete':
+    del mcbSelf[sys.argv[2]]
 elif len(sys.argv) == 2:
     # list keywords and load content
     if sys.argv[1].lower() == 'list':
         pyperclip.copy(str(list(mcbSelf.keys())))
+    # delete all
+    elif sys.argv[1].lower() == 'deleteall':
+        mcbSelf.clear()
     elif sys.argv[1] in mcbSelf:
         pyperclip.copy(mcbSelf[sys.argv[1]])
 mcbSelf.close()
